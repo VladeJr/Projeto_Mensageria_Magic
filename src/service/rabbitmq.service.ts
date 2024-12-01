4d9ee4dd8127d96eb7e162401218dcc0221914bc
João Gonçalves <115425699+joaoarteagle@users.noreply.github.com>
2024-12-01 22:17:44 +0000

    implementação do logger

--- src/service/rabbitmq.service.ts
+++ src/service/rabbitmq.service.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { Inject } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { LoggingService } from './logging.service';
 
 @Injectable()
 export class RabbitMQService {
-  constructor(@Inject('RABBITMQ_SERVICE') private readonly client: ClientProxy) {}
+  constructor(
+    @Inject('RABBITMQ_SERVICE') private readonly client: ClientProxy,
+    private readonly loggingService: LoggingService
+  ) {}
 
   async sendMessage(queue: string, message: any, options: any = {}): Promise<void> {
-    await this.client.emit(queue, { message, options }).toPromise();
+    this.loggingService.log(`Sending message to queue: ${queue}`,'RabbitMQService');
+    try {
+      await this.client.emit(queue, { message, options }).toPromise();
+      this.loggingService.log(`Message sent to queue: ${queue}`, 'RabbitMQService');
+    } catch (error) {
+      this.loggingService.error(`Failed to send message to queue: ${queue}` , error.stack, 'RabbitMQService');
+      throw error;
+    }
   }
 }
