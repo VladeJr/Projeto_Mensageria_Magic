a62bf47f81d2c6dc6ffc2ebadf265456768db71f
Pedro Henrique Gomes <130695589+Pedrogom7@users.noreply.github.com>
2024-12-01 23:10:00 -0300

    melhorando roles.decorator

--- src/roles/roles.decorator.ts
+++ src/roles/roles.decorator.ts
@@ -1,5 +1,8 @@
 import { SetMetadata } from '@nestjs/common';
 import { Role } from './enums/functions.enum';
 
+
+//exporting roles and keys 
 export const ROLES_KEY = 'roles';
 export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
+
